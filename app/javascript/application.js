ecf9ae593453d3229ff1673d6e6f01829c38fde4
AsimKhan2019 <asim.mahmood.khan@hotmail.com>
2023-07-01 13:18:42 +0500

    updating some files towards dashboard development

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,15 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+import "core/popper.min.js"
+import "core/bootstrap.min.js"
+import "core/bootstrap.bundle.min.js"
+
+import "plugins/perfect-scrollbar.min.js"
+import "plugins/smooth-scrollbar.min.js"
+import "plugins/chartjs.min.js"
+import "plugins/bootstrap-notify.js"
+import "plugins/Chart.extension.js"
+import "plugins/countup.min.js"
+import "plugins/world.js"
