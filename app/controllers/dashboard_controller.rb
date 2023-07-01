345aae3c0bc33668e38365213373c08c912ef3bd
AsimKhan2019 <asim.mahmood.khan@hotmail.com>
2023-07-01 16:41:12 +0500

    updated code

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,17 @@
 class DashboardController < ApplicationController
-  def index
-  end
+  def index; end
+
+  def user_profile; end
+
+  def table_list; end
+
+  def typography; end
+
+  def icons; end
+
+  def maps; end
+
+  def notifications; end
+
+  def rtl_support; end
 end
